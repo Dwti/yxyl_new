e1f80126c516ae6dae79d0c603aeee36c46e466d
戴延枫 <daiyanfeng@autohome.com.cn>
2017-05-16 15:24:31 +0800

    在答题activity里添加切换上一题下一题方法

--- phone/app/src/main/java/com/yanxiu/gphone/student/homework/questions/activity/AnswerQuestionActivity.java
+++ phone/app/src/main/java/com/yanxiu/gphone/student/homework/questions/activity/AnswerQuestionActivity.java
@@ -3,6 +3,7 @@ package com.yanxiu.gphone.student.homework.questions.activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -11,6 +12,7 @@ import com.yanxiu.gphone.student.homework.questions.adapter.QAViewPagerAdapter;
 import com.yanxiu.gphone.student.homework.questions.fragment.AnswerCardFragment;
 import com.yanxiu.gphone.student.homework.questions.fragment.ComplexExerciseFragmentBase;
 import com.yanxiu.gphone.student.homework.questions.fragment.ExerciseFragmentBase;
+import com.yanxiu.gphone.student.homework.questions.fragment.SimpleExerciseFragmentBase;
 import com.yanxiu.gphone.student.homework.questions.model.BaseQuestion;
 import com.yanxiu.gphone.student.homework.questions.view.QAViewPager;
 
@@ -22,6 +24,7 @@ import java.util.ArrayList;
 public class AnswerQuestionActivity extends AppCompatActivity implements AnswerCardFragment.OnCardItemSelectListener {
     private static final String EXTRA_NODES = "extra mNodes";
 
+    private FragmentManager mFragmentManager;
     private QAViewPager mViewPager;
     private QAViewPagerAdapter mAdapter;
     private ArrayList<BaseQuestion> mNodes;
@@ -40,10 +43,10 @@ public class AnswerQuestionActivity extends AppCompatActivity implements AnswerC
     }
 
     private void initView() {
-        final FragmentManager fm = getSupportFragmentManager();
+        mFragmentManager = getSupportFragmentManager();
         mViewPager = (QAViewPager) findViewById(R.id.vp_viewPager);
         mViewPager.setOffscreenPageLimit(1);
-        mAdapter = new QAViewPagerAdapter(fm);
+        mAdapter = new QAViewPagerAdapter(mFragmentManager);
 //        mViewPager.setFragmentManager(fm);
         mAdapter.setData(mNodes);
         mViewPager.setAdapter(mAdapter);
@@ -52,22 +55,14 @@ public class AnswerQuestionActivity extends AppCompatActivity implements AnswerC
             @Override
             public void onSwipeOutAtEnd() {
                 Log.e("TAG", "Outter swipe out at end");
-                // 可以在这里打个断点，所有Fill Blank的答案均已存入nodes里
-                if (mCardFragment == null) {
-                    mCardFragment = new AnswerCardFragment();
-                    mCardFragment.nodes = allNodesThatHasNumber();
-                    mCardFragment.setOnCardItemSelectListener(AnswerQuestionActivity.this);
-                }
-                if (fm.findFragmentById(R.id.fragment_card) == null) {
-                    // 有一个滑动event的问题，导致card fragment上能滑动下面的viewpager，可能导致重复加载card getFragment
-                    fm.beginTransaction().add(R.id.fragment_card, mCardFragment).commit();
-                }
+                showAnswerCardFragment();
             }
         });
     }
 
     /**
      * 给答题卡设置题号
+     *
      * @return
      */
     private ArrayList<BaseQuestion> allNodesThatHasNumber() {
@@ -103,5 +98,127 @@ public class AnswerQuestionActivity extends AppCompatActivity implements AnswerC
 //            ComplexExerciseFragmentBase f = (ComplexExerciseFragmentBase) a.instantiateItem(mViewPager, index);
 //            f.setChildrenPositionRecursively(remainPositions);
 //        }
+    }
+
+    /**
+     * 显示答题卡
+     */
+    private void showAnswerCardFragment() {
+        // 可以在这里打个断点，所有Fill Blank的答案均已存入nodes里
+        if (mCardFragment == null) {
+            mCardFragment = new AnswerCardFragment();
+            mCardFragment.nodes = allNodesThatHasNumber();
+            mCardFragment.setOnCardItemSelectListener(AnswerQuestionActivity.this);
+        }
+        if (mFragmentManager.findFragmentById(R.id.fragment_card) == null) {
+            // 有一个滑动event的问题，导致card fragment上能滑动下面的viewpager，可能导致重复加载card getFragment
+            mFragmentManager.beginTransaction().add(R.id.fragment_card, mCardFragment).commit();
+        }
+    }
+
+    /**
+     * 切换下一题目
+     */
+    public void nextQuestion() {
+        ExerciseFragmentBase currentFramgent = null;//当前的Fragment
+        FragmentStatePagerAdapter adapter;
+        int index;//当前Fragment在外层viewPager中的index
+        int size;//viewPager的总共的size
+        try {
+            adapter = (FragmentStatePagerAdapter) mViewPager.getAdapter();
+            index = mViewPager.getCurrentItem();
+            currentFramgent = (ExerciseFragmentBase) adapter.instantiateItem(mViewPager, index);
+            size = adapter.getCount();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        if (adapter == null || index < 0 || size < 1 || mViewPager == null || currentFramgent == null)
+            return;
+
+        if (currentFramgent instanceof ComplexExerciseFragmentBase) {
+            ComplexExerciseFragmentBase complexExerciseFragment = (ComplexExerciseFragmentBase) currentFramgent;
+            ViewPager innerViewPager = complexExerciseFragment.getViewPager();
+            FragmentStatePagerAdapter innerAdapter = (FragmentStatePagerAdapter) innerViewPager.getAdapter();
+            int innerIndex = innerViewPager.getCurrentItem();
+            int innerSize = innerViewPager.getAdapter().getCount();
+
+            if (complexExerciseFragment == null || innerViewPager == null || innerAdapter == null || innerIndex < 0 || innerSize < 1)
+                return;
+            /**
+             * 复合题型，切换下一题，共有三种状态：
+             * 1.正常切换下一小题
+             * 2.处在最后一个小题时，且外部大题不是最后一道题，那么外部大题进入下一题
+             * 3.处在最后一个小题，且外部大题也是最后一题，那么判断为是最后一道题，展现答题卡
+             */
+            if (innerIndex == (innerSize - 1) && index == (size - 1)) {
+                //状态3
+                showAnswerCardFragment();
+            } else if (innerIndex == (innerSize - 1)) {
+                //状态2
+                mViewPager.setCurrentItem(index + 1);
+            } else {
+                //状态1
+                innerViewPager.setCurrentItem(innerIndex + 1);
+            }
+
+        } else if (currentFramgent instanceof SimpleExerciseFragmentBase) {
+            if (index == (size - 1)) {
+                //最后一题,展示答题卡
+                showAnswerCardFragment();
+            } else {
+                //下一题
+                mViewPager.setCurrentItem(index + 1);
+            }
+        }
+
+    }
+
+    /**
+     * 切换上一题目
+     */
+    public void previousQuestion() {
+        ExerciseFragmentBase currentFramgent = null;//当前的Fragment
+        FragmentStatePagerAdapter adapter;
+        int index;//当前Fragment在外层viewPager中的index
+        int size;//viewPager的总共的size
+        try {
+            adapter = (FragmentStatePagerAdapter) mViewPager.getAdapter();
+            index = mViewPager.getCurrentItem();
+            currentFramgent = (ExerciseFragmentBase) adapter.instantiateItem(mViewPager, index);
+            size = adapter.getCount();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        if (adapter == null || index < 0 || size < 1 || mViewPager == null || currentFramgent == null)
+            return;
+
+        if (currentFramgent instanceof ComplexExerciseFragmentBase) {
+            ComplexExerciseFragmentBase complexExerciseFragment = (ComplexExerciseFragmentBase) currentFramgent;
+            ViewPager innerViewPager = complexExerciseFragment.getViewPager();
+            FragmentStatePagerAdapter innerAdapter = (FragmentStatePagerAdapter) innerViewPager.getAdapter();
+            int innerIndex = innerViewPager.getCurrentItem();
+            int innerSize = innerViewPager.getAdapter().getCount();
+
+            if (complexExerciseFragment == null || innerViewPager == null || innerAdapter == null || innerIndex < 0 || innerSize < 1)
+                return;
+
+            if (innerIndex >= 1) {
+                //小题不是第一题时,有上一小题
+                innerViewPager.setCurrentItem(innerIndex - 1);
+            } else if (index >= 1) {
+                //外部大题不是第一题时,有上一大题
+                mViewPager.setCurrentItem(index - 1);
+            }
+
+        } else if (currentFramgent instanceof SimpleExerciseFragmentBase) {
+            if (index >= 1) {
+                //不是第一大题时,有上一题
+                mViewPager.setCurrentItem(index - 1);
+            }
+        }
     }
 }
