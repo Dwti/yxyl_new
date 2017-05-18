222401be0e3bf62e87d55e461be30b2a78d6dd70
sunpeng <sunpeng@yanxiu.com>
2017-05-18 10:20:59 +0800

    新增加入班级相关页面

---
+++ phone/app/src/main/java/com/yanxiu/gphone/student/homework/classmanage/JoinClassActivity.java
@@ -0,0 +1,20 @@
+package com.yanxiu.gphone.student.homework.classmanage;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.yanxiu.gphone.student.R;
+
+/**
+ * Created by sp on 17-5-17.
+ */
+
+public class JoinClassActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_class);
+    }
+}
