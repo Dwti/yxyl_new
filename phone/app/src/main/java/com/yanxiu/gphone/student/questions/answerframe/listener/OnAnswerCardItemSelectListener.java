7f7ae0b2f6520e50842ed72c0699aba1222208c1
戴延枫 <daiyanfeng@autohome.com.cn>
2017-06-07 09:34:59 +0800

    添加答题卡部分功能

---
+++ phone/app/src/main/java/com/yanxiu/gphone/student/questions/answerframe/listener/OnAnswerCardItemSelectListener.java
@@ -0,0 +1,12 @@
+package com.yanxiu.gphone.student.questions.answerframe.listener;
+
+import com.yanxiu.gphone.student.questions.answerframe.bean.BaseQuestion;
+
+/**
+ * Created by 戴延枫 on 2017/6/6.
+ * 答题卡点击回调
+ */
+
+public interface OnAnswerCardItemSelectListener {
+    void onItemSelect(BaseQuestion item);
+}
