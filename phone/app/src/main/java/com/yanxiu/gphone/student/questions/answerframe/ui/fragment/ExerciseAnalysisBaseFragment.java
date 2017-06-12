f5817d1dca3fc260851180a27391b9d7c454847e
戴延枫 <daiyanfeng@autohome.com.cn>
2017-06-12 11:45:54 +0800

    添加解析框架demo逻辑

---
+++ phone/app/src/main/java/com/yanxiu/gphone/student/questions/answerframe/ui/fragment/ExerciseAnalysisBaseFragment.java
@@ -0,0 +1,11 @@
+package com.yanxiu.gphone.student.questions.answerframe.ui.fragment;
+
+/**
+ * Created by 戴延枫 on 2017/5/5.
+ * 解析Frament基类
+ */
+
+public abstract class ExerciseAnalysisBaseFragment extends ExerciseBaseFragment  {
+    public final String TAG = this.getClass().getSimpleName();
+
+}
