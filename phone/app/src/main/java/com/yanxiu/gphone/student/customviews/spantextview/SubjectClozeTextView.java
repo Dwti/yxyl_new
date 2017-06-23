7fef3b5b3b546686cafce80310d5f3f2c6c5577d
sunpeng <sunpeng@yanxiu.com>
2017-06-23 18:03:21 +0800

    添加主观填空题解析控件

---
+++ phone/app/src/main/java/com/yanxiu/gphone/student/customviews/spantextview/SubjectClozeTextView.java
@@ -0,0 +1,64 @@
+package com.yanxiu.gphone.student.customviews.spantextview;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.AttrRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.annotation.StyleRes;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.List;
+
+/**
+ * Created by sunpeng on 2017/6/23.
+ */
+
+public class SubjectClozeTextView extends ReplacementSpanTextView<SubjectClozeView> implements OnReplaceCompleteListener {
+    public SubjectClozeTextView(@NonNull Context context) {
+        super(context);
+        init();
+    }
+
+    public SubjectClozeTextView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public SubjectClozeTextView(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public SubjectClozeTextView(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr, @StyleRes int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init(){
+        setOnReplaceCompleteListener(this);
+    }
+
+    @Override
+    public void onReplaceComplete() {
+        List<SubjectClozeView> views = getReplaceViews();
+        if(views == null || views.size() ==0){
+            return;
+        }
+        if(views.size() == 1){
+            views.get(0).setNumberVisible(View.GONE);
+            return;
+        }
+        for(int i =0; i< views.size(); i++){
+            views.get(i).setTextNumber(i+1);
+        }
+    }
+
+    @Override
+    protected SubjectClozeView getView() {
+        return new SubjectClozeView(getContext());
+    }
+}
---
+++ phone/app/src/main/java/com/yanxiu/gphone/student/customviews/spantextview/SubjectClozeView.java
@@ -0,0 +1,59 @@
+package com.yanxiu.gphone.student.customviews.spantextview;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.AttrRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.annotation.StyleRes;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.yanxiu.gphone.student.R;
+
+/**
+ * Created by sunpeng on 2017/6/23.
+ */
+
+public class SubjectClozeView extends FrameLayout {
+
+    private TextView mNumber;
+
+    public SubjectClozeView(@NonNull Context context) {
+        super(context);
+    }
+
+    public SubjectClozeView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SubjectClozeView(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public SubjectClozeView(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr, @StyleRes int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    private void initView(Context context){
+        View view = LayoutInflater.from(context).inflate(R.layout.cloze_view,this,true);
+        mNumber = (TextView) view.findViewById(R.id.text_number);
+    }
+
+    public void setTextNumber(int num){
+        mNumber.setText(String.valueOf(num));
+    }
+
+    public int getTextNumber(){
+        return Integer.parseInt(mNumber.getText().toString());
+    }
+
+    public void setNumberVisible(int visible){
+        mNumber.setVisibility(visible);
+    }
+}
