3e4ce93d7dda7b29fac6bec34424b688e2d3bf5c
戴延枫 <daiyanfeng@autohome.com.cn>
2017-07-10 15:52:52 +0800

    填空题解析添加逾期未提交显示view逻辑

--- phone/app/src/main/java/com/yanxiu/gphone/student/questions/yesno/YesNoAnalysisFragment.java
+++ phone/app/src/main/java/com/yanxiu/gphone/student/questions/yesno/YesNoAnalysisFragment.java
@@ -5,7 +5,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.text.Html;
 import android.text.Spanned;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
