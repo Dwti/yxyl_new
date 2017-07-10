7bd2cf950e6a49287f553c1280553c955015efd1
chenweiqi <chenweiqi@yanxiu.com>
2017-07-10 16:18:41 +0800

    clear test data

--- phone/app/src/main/java/com/yanxiu/gphone/student/customviews/ListenerSeekBarLayout.java
+++ phone/app/src/main/java/com/yanxiu/gphone/student/customviews/ListenerSeekBarLayout.java
@@ -53,8 +53,8 @@ public class ListenerSeekBarLayout extends LinearLayout implements SeekBar.OnSee
     private int mMax=0;
     private int mProgress=0;
 
-//    private String mUrl;
-        private String mUrl="http://data.5sing.kgimg.com/G034/M05/16/17/ApQEAFXsgeqIXl7gAAVVd-n31lcAABOogKzlD4ABVWP363.mp3";
+    private String mUrl;
+//        private String mUrl="http://data.5sing.kgimg.com/G034/M05/16/17/ApQEAFXsgeqIXl7gAAVVd-n31lcAABOogKzlD4ABVWP363.mp3";
     private long mMoveDown;
 
     public ListenerSeekBarLayout(Context context) {
@@ -229,7 +229,7 @@ public class ListenerSeekBarLayout extends LinearLayout implements SeekBar.OnSee
         if (TextUtils.isEmpty(url)) {
             return;
         }
-//        this.mUrl = url;
+        this.mUrl = url;
     }
 
     public void setPause() {
