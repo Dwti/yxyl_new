c62620e842190caeafa5c3e9c0045b9ab7fef6a8
sunpeng <sunpeng@yanxiu.com>
2017-08-24 14:23:09 +0800

    add log

--- phone/app/src/main/java/com/yanxiu/gphone/student/base/YanxiuBaseFragment.java
+++ phone/app/src/main/java/com/yanxiu/gphone/student/base/YanxiuBaseFragment.java
@@ -1,10 +1,18 @@
 package com.yanxiu.gphone.student.base;
 
 import android.support.v4.app.Fragment;
+import android.util.Log;
+
+import com.yanxiu.gphone.student.constant.Constants;
 
 /**
  * Created by 戴延枫 on 2017/5/9.
  */
 public class YanxiuBaseFragment extends Fragment {
-    private final String TAG = "YanxiuBaseFragment";
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(Constants.TAG,this.getClass().getName());
+    }
 }
