5b8fdce457ced0a8363b375320496280349eebfd
chenweiqi <chenweiqi@yanxiu.com>
2017-08-29 14:02:13 +0800

    add translation anim

--- phone/app/src/main/java/com/yanxiu/gphone/student/user/userinfo/dialog/EditorHeadImgDialog.java
+++ phone/app/src/main/java/com/yanxiu/gphone/student/user/userinfo/dialog/EditorHeadImgDialog.java
@@ -4,10 +4,15 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v4.view.ViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.BounceInterpolator;
 
 import com.yanxiu.gphone.student.R;
+import com.yanxiu.gphone.student.util.ScreenUtils;
+import com.yanxiu.gphone.student.util.anim.JumpAnimManager;
+import com.yanxiu.gphone.student.util.anim.TranslationYAnimUtil;
 
 /**
  * Created by Canghaixiao.
@@ -15,6 +20,12 @@ import com.yanxiu.gphone.student.R;
  * Function :
  */
 public class EditorHeadImgDialog extends Dialog implements View.OnClickListener {
+
+    private View mAnimView;
+    private Context mContext;
+    private int mTranslationY=800;
+    private int mAnimTime=200;
+    private View mAnimBgview;
 
     public interface OnViewClickListener{
         void onAlbumClick();
@@ -30,12 +41,22 @@ public class EditorHeadImgDialog extends Dialog implements View.OnClickListener 
     }
 
     private void init(Context context){
-        View view= LayoutInflater.from(context).inflate(R.layout.dialog_editorheadimg,null);
-        setContentView(view);
-        view.setOnClickListener(this);
-        view.findViewById(R.id.ll_album).setOnClickListener(this);
-        view.findViewById(R.id.ll_camera).setOnClickListener(this);
-        view.findViewById(R.id.tv_cancle).setOnClickListener(this);
+        mContext=context;
+        View rootView= LayoutInflater.from(context).inflate(R.layout.dialog_editorheadimg,null);
+        setContentView(rootView);
+        rootView.setOnClickListener(this);
+        rootView.findViewById(R.id.ll_album).setOnClickListener(this);
+        rootView.findViewById(R.id.ll_camera).setOnClickListener(this);
+        rootView.findViewById(R.id.tv_cancle).setOnClickListener(this);
+
+        mAnimBgview=rootView.findViewById(R.id.gradation);
+        mAnimView=rootView.findViewById(R.id.rl_headimg);
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        TranslationYAnimUtil.getInstence().setViewHeight(mContext,R.dimen.userinfo_dialog_h).setBgGradation(mAnimBgview,0f,0.7f).setStartAnim(mAnimView);
     }
 
     public void setClickListener(OnViewClickListener onViewClickListener){
---
+++ phone/app/src/main/java/com/yanxiu/gphone/student/util/anim/TranslationYAnimUtil.java
@@ -0,0 +1,152 @@
+package com.yanxiu.gphone.student.util.anim;
+
+import android.content.Context;
+import android.support.annotation.DimenRes;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPropertyAnimatorListener;
+import android.support.v4.view.ViewPropertyAnimatorUpdateListener;
+import android.view.View;
+
+import com.yanxiu.gphone.student.util.ScreenUtils;
+
+/**
+ * Created by Canghaixiao.
+ * Time : 2017/8/29 12:16.
+ * Function :
+ */
+public class TranslationYAnimUtil {
+
+    private static final int DEFAULT_DURATION = 200;
+
+    private static TranslationYAnimUtil translationYAnimUtil;
+    private TranslationAnim translationAnim = new TranslationAnim();
+    private TranslationAnimUpdata translationAnimUpdata = new TranslationAnimUpdata();
+    private int mTranslationY = 0;
+    private int mDuration = 0;
+
+    private TranslationYAnimUtil() {
+        translationAnim.clear();
+        translationAnimUpdata.clear();
+    }
+
+    public static TranslationYAnimUtil getInstence() {
+        translationYAnimUtil = new TranslationYAnimUtil();
+        return translationYAnimUtil;
+    }
+
+    public TranslationYAnimUtil setViewHeight(Context context, @DimenRes int resId) {
+        mTranslationY = context.getResources().getDimensionPixelSize(resId);
+        int windowHeight = ScreenUtils.getScreenHeight(context);
+        mDuration = DEFAULT_DURATION;
+        mDuration = mDuration * (windowHeight / mTranslationY);
+        return translationYAnimUtil;
+    }
+
+    public TranslationYAnimUtil setDuration(Context context, int duration) {
+        this.mDuration = duration;
+        int windowHeight = ScreenUtils.getScreenHeight(context);
+        mDuration = mDuration * (windowHeight / mTranslationY);
+        return translationYAnimUtil;
+    }
+
+    public TranslationYAnimUtil setBgGradation(View view, float start, float end) {
+        translationAnimUpdata.setParame(view, start, end, mTranslationY);
+        translationAnim.setParame(view, start, end,translationAnimUpdata);
+        return translationYAnimUtil;
+    }
+
+    public void setStartAnim(View view) {
+        ViewCompat.setTranslationY(view, mTranslationY);
+
+        ViewCompat.animate(view)
+                .translationY(0)
+                .setDuration(mDuration)
+                .setUpdateListener(translationAnimUpdata)
+                .setListener(translationAnim);
+    }
+
+    private static class TranslationAnim implements ViewPropertyAnimatorListener {
+
+        private View mWindowView;
+        private TranslationAnimUpdata mAnimUpdata;
+        private float mStart;
+        private float mEnd;
+
+        private TranslationAnim() {
+
+        }
+
+        private void setParame(View view, float start, float end,TranslationAnimUpdata animUpdata) {
+            this.mWindowView = view;
+            this.mAnimUpdata=animUpdata;
+            this.mStart = start;
+            this.mEnd = end;
+        }
+
+        public void clear() {
+            this.mWindowView = null;
+            this.mStart = 0f;
+            this.mEnd = 0f;
+        }
+
+        @Override
+        public void onAnimationStart(View view) {
+            if (mWindowView != null) {
+                mWindowView.setAlpha(mStart);
+            }
+        }
+
+        @Override
+        public void onAnimationEnd(View view) {
+            if (mWindowView != null) {
+                mWindowView.setAlpha(mEnd);
+            }
+            clear();
+            mAnimUpdata.clear();
+        }
+
+        @Override
+        public void onAnimationCancel(View view) {
+            clear();
+            mAnimUpdata.clear();
+        }
+    }
+
+    private static class TranslationAnimUpdata implements ViewPropertyAnimatorUpdateListener {
+
+        private View mWindowView;
+        private float mStart;
+        private float mEnd;
+        private int mTranslationY;
+
+        private TranslationAnimUpdata() {
+
+        }
+
+        private void setParame(View view, float start, float end, int translationY) {
+            this.mWindowView = view;
+            this.mStart = start;
+            this.mEnd = end;
+            this.mTranslationY = translationY;
+        }
+
+        public void clear() {
+            this.mWindowView = null;
+            this.mStart = 0f;
+            this.mEnd = 0f;
+            this.mTranslationY = 0;
+        }
+
+        @Override
+        public void onAnimationUpdate(final View view) {
+            if (mWindowView != null) {
+                int translationY = (int) view.getTranslationY();
+//                        Logger.d("anim",translationY+"");
+                float ratio = ((float) Math.abs(mTranslationY) - (float) Math.abs(translationY)) / (float) Math.abs(mTranslationY);
+//                        Logger.d("anim",(mEnd - mStart) * ratio + mStart+"");
+                mWindowView.setAlpha((mEnd - mStart) * ratio + mStart);
+            }
+        }
+    }
+
+}
