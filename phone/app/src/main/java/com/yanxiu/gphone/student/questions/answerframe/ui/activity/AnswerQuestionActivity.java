bee0845fc5dbccb2299d2770a8e28eb3e7153db7
sunpeng <sunpeng@yanxiu.com>
2017-10-31 15:35:34 +0800

    fix bug mes-1921

--- phone/app/src/main/java/com/yanxiu/gphone/student/questions/answerframe/ui/activity/AnswerQuestionActivity.java
+++ phone/app/src/main/java/com/yanxiu/gphone/student/questions/answerframe/ui/activity/AnswerQuestionActivity.java
@@ -659,11 +659,15 @@ public class AnswerQuestionActivity extends YanxiuBaseActivity implements View.O
                 expandVideo();
                 break;
             case R.id.iv_collapse:
-                collapseVideo();
+                if(layout_cover.getVisibility() == View.VISIBLE){
+                    layout_cover.setVisibility(View.GONE);
+                    video_collapse.setVisibility(View.GONE);
+                }else {
+                    collapseVideo();
+                }
                 break;
             case R.id.iv_play:
                 mPlayerView.setVisibility(View.VISIBLE);
-                video_collapse.setVisibility(View.VISIBLE);
                 layout_cover.setVisibility(View.GONE);
                 playVideo();
                 break;
@@ -922,6 +926,7 @@ public class AnswerQuestionActivity extends YanxiuBaseActivity implements View.O
    //视频播放部分
 
     private void expandVideo(){
+        video_collapse.setVisibility(View.VISIBLE);
         layout_cover.setVisibility(View.VISIBLE);
     }
 
