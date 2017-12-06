0fd5127376c25016684e04de5401eb8a5b5deb95
sunpeng <sunpeng@yanxiu.com>
2017-12-06 12:38:32 +0800

    临时提交

--- phone/app/src/main/java/com/yanxiu/gphone/student/mistakeredo/adapter/WrongQPointAdapter.java
+++ phone/app/src/main/java/com/yanxiu/gphone/student/mistakeredo/adapter/WrongQPointAdapter.java
@@ -2,6 +2,7 @@ package com.yanxiu.gphone.student.mistakeredo.adapter;
 
 import android.support.v7.widget.RecyclerView;
 
+import com.yanxiu.gphone.student.R;
 import com.yanxiu.gphone.student.exercise.adapter.ExerciseExpandableAdapter;
 import com.yanxiu.gphone.student.mistakeredo.bean.WrongQPointBean;
 
@@ -20,6 +21,8 @@ public class WrongQPointAdapter extends ExerciseExpandableAdapter<WrongQPointBea
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         super.onBindViewHolder(holder, position);
-        ((ExpandableViewHolder)holder).text.setText(mData.get(position).getName());
+        String title = String.format(holder.itemView.getContext().getString(R.string.point_title),mData.get(position).getName(),mData.get(position).getQuestion_num());
+//        ((ExpandableViewHolder)holder).text.setText(mData.get(position).getName());
+        ((ExpandableViewHolder)holder).text.setText(title);
     }
 }
