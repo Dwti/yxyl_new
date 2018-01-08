8c85ce5ea8c6d1a61a02872f660c9f7235dc419a
chenweiqi <chenweiqi@yanxiu.com>
2018-01-08 10:33:44 +0800

    set itembean extends basebean

--- phone/app/src/main/java/com/yanxiu/gphone/student/questions/connect/ConnectAnalysisItemBean.java
+++ phone/app/src/main/java/com/yanxiu/gphone/student/questions/connect/ConnectAnalysisItemBean.java
@@ -1,11 +1,13 @@
 package com.yanxiu.gphone.student.questions.connect;
 
+import com.yanxiu.gphone.student.base.BaseBean;
+
 /**
  * Created by Canghaixiao.
  * Time : 2017/12/26 17:31.
  * Function :
  */
-public class ConnectAnalysisItemBean {
+public class ConnectAnalysisItemBean extends BaseBean {
 
     public String text;
     public boolean isExtra;
