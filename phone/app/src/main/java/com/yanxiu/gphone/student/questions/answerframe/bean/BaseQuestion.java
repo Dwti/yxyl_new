ae4bf69df3e1a924dec317ccd7b2ebd24d632c5d
sunpeng <sunpeng@yanxiu.com>
2018-01-19 17:27:21 +0800

    操作题题号按照计算解答处理

--- phone/app/src/main/java/com/yanxiu/gphone/student/questions/answerframe/bean/BaseQuestion.java
+++ phone/app/src/main/java/com/yanxiu/gphone/student/questions/answerframe/bean/BaseQuestion.java
@@ -664,7 +664,7 @@ public abstract class BaseQuestion implements Serializable {
      */
     public Boolean isNodeCountForTotal() {
 //        if (template.equals(QuestionTemplate.CLOZE)) {
-        if (isComplexQuestion && !type_id.equals("8") && !type_id.equals("22") && !type_id.equals("24") && !type_id.equals("30")) {
+        if (isComplexQuestion && !type_id.equals("8") && !type_id.equals("22") && !type_id.equals("24") && !type_id.equals("30") && !type_id.equals("25")) {
             return false;
         }
         return true;
@@ -680,7 +680,7 @@ public abstract class BaseQuestion implements Serializable {
      */
     public Boolean isChildNodeCountForTotal() {
 //        if (template.equals(QuestionTemplate.CLOZE)) {
-        if (isComplexQuestion && !type_id.equals("8") && !type_id.equals("22") && !type_id.equals("24") && !type_id.equals("30")) {
+        if (isComplexQuestion && !type_id.equals("8") && !type_id.equals("22") && !type_id.equals("24") && !type_id.equals("30") && !type_id.equals("25")) {
             return true;
         }
 
